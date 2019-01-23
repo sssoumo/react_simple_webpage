f8374d84a1f8bacd930458a9ceb42bdbe9c5a498
Saadman <sssoumo@gmail.com>
2019-01-24 00:02:35 +0600

    added event section

---
+++ src/components/venueNfo/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import icon_calendar from '../../resources/images/icons/calendar.png'
+import icon_location from '../../resources/images/icons/location.png'
+
+const VenueNfo = () => {
+    return (
+        <div className="bck_black">
+            <div className="center_wrapper">
+                <div className="vn_wrapper">
+
+                    <div className="vn_item">
+                        <div className="vn_outer">
+                            <div className="vn_inner">
+                                <div className="vn_icon_square bck_red">
+
+                                </div>
+
+                                <div className="vn_icon"
+                                    style={{
+                                        background:`url(${icon_calendar})`
+                                    }}
+                                ></div>
+                                <div className="vn_title">
+                                    Event Date & Time
+                                </div>
+                                <div className="vn_desc">
+                                    7 August 2017 @10.00pm
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="vn_item">
+                        <div className="vn_outer">
+                            <div className="vn_inner">
+                                <div className="vn_icon_square bck_red">
+
+                                </div>
+
+                                <div className="vn_icon"
+                                    style={{
+                                        background:`url(${icon_location})`
+                                    }}
+                                ></div>
+                                <div className="vn_title">
+                                    Event Location
+                                </div>
+                                <div className="vn_desc">
+                                    7 August 2017 @10.00pm
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default VenueNfo;
