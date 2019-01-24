55baac4fde4636052b5da4c8d27e3701ccc194fe
Saadman <sssoumo@gmail.com>
2019-01-24 22:29:01 +0600

    Highlights section added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './resources/styles.css';
 import Header from './components/header_footer/Header'
 import Featured from './components/featured';
 import VenueNfo from './components/venueNfo'
+import Highlights from './components/Highlights'
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <Header/>
         <Featured/>
         <VenueNfo/>
+        <Highlights/>
       </div>
     );
   }
---
+++ src/components/Highlights/Discount.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
+import Slide from 'react-reveal/Slide';
+
+class Discount extends Component {
+
+    state={
+        discountStart:0,
+        discountEnd:30
+    }
+
+    porcentage=()=>{
+        if(this.state.discountStart < this.state.discountEnd){
+            this.setState({
+                discountStart: this.state.discountStart+1
+            })
+        }
+    }
+
+    componentDidUpdate(){
+        setTimeout(()=>{
+            this.porcentage()
+        },30)
+    }
+
+    render() {
+        return (
+            <div className="center_wrapper">
+                 <div className="discount_wrapper">
+                
+                    <Fade
+                        onReveal={()=>this.porcentage()}
+                    >
+                        <div className="discount_porcentage">
+                            <span>{this.state.discountStart}%</span>
+                            <span>Off</span>
+                        </div>
+                    </Fade>
+                    
+                    <Slide right>
+                        <div className="discount_description">
+                            <h3>Purchase tickets before 20th JUNE</h3>
+                            <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+                        </div>
+                    </Slide>
+                    
+
+                    <div>
+                        button
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Discount;
