f29cc3a0c662083be6fbf180f946e7543658aacd
Saadman <sssoumo@gmail.com>
2019-01-25 22:17:37 +0600

    reusable button added

--- src/components/Highlights/Discount.js
+++ src/components/Highlights/Discount.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import Slide from 'react-reveal/Slide';
 
+import MyButton from '../utils/MyButton';
+
 class Discount extends Component {
 
     state={
@@ -41,13 +43,15 @@ class Discount extends Component {
                         <div className="discount_description">
                             <h3>Purchase tickets before 20th JUNE</h3>
                             <p>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+                            <MyButton
+                                text="Purchase tickets"
+                                bck="#ffa800"
+                                color="#ffffff"
+                                link="google.com"
+                            />
                         </div>
+                        
                     </Slide>
-                    
-
-                    <div>
-                        button
-                    </div>
                 </div>
             </div>
         );
